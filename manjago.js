1884499fa24e096644555a4afa1bfc7335d577c0
Темненков Кирилл Дмитриевич <kirill@temnenkov.com>
2023-01-29 11:17:19 +0300

    tryMoce

--- manjago.js
+++ manjago.js
@@ -1,5 +1,29 @@
 global turn = 0;
+
 turn += 1;
 debug("==== Turn " + turn);
+
+var enemy = realEnemy() 
+
+debug("==== Movement phase");
+tryMove(enemy);
+
 debug(getOperations() + " operations");
 
+
+function realEnemy() {
+    var pretenders = getAliveEnemies()
+    var realEnemies = arrayFilter(pretenders, function (v) {
+        return getType(v) != ENTITY_CHEST
+    })
+
+    if (isEmpty(realEnemies)) {
+        return getNearestEnemy()
+    } else {
+        return realEnemies[0]
+    }
+}
+
+function tryMove(enemy) {
+    moveToward(enemy)
+}
